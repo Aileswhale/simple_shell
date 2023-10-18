094352bc1e5c50fc43343500211032dbaf041759
GodwinCyber <godwin032@gmail.com>
2023-10-18 15:26:42 -0700

    modifief=d ciode

--- allocted_memory.c
+++ allocted_memory.c
@@ -1,7 +1,7 @@
 #include "aileswhale.h"
 
 /**
- * _realloc - Doubles the space allocated for a pointer.
+ * realloc - Doubles the space allocated for a pointer.
  * @pointer: Pointer to the original array.
  * @sizes: Pointer to the number of elements in the original array.
  *
